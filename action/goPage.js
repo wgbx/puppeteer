546017817e0850a5127196160ab4856c58ca9d5f
微光北下 <wgbx@foxmail.com>
2025-02-24 14:58:31 +0800

    feat: add debug

--- action/goPage.js
+++ action/goPage.js
@@ -15,6 +15,7 @@ export async function goPage(browser) {
 export async function goJD(browser) {
   const page = await browser.newPage({})
   await page.setViewport({ width: 1920, height: 1080 })
+  console.log('🚀 ~ goJD ~ page:', process.env.JD_COOKIE_VALUEage)
   await page.setCookie({ name: JD_COOKIE_KEY, value: process.env.JD_COOKIE_VALUE, domain: JD_COOKIE_DOMAIN })
   await page.goto(JD_SIGN_PAGE)
   const signButton = await page.waitForSelector(JD_SIGN_BUTTON)
