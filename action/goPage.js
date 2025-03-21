7496741ed2ccd8d7df5023c56efed6e12f502f12
微光北下 <wgbx@foxmail.com>
2025-03-21 10:43:30 +0800

    feat: add process

--- action/goPage.js
+++ action/goPage.js
@@ -17,9 +17,12 @@ export async function goJD(browser) {
   await page.setViewport({ width: 1920, height: 1080 })
   await page.setCookie({ name: JD_COOKIE_KEY, value: process.env.JD_COOKIE_VALUE, domain: JD_COOKIE_DOMAIN })
   await page.goto(JD_SIGN_PAGE)
-  const signButton = await page.waitForSelector(JD_SIGN_BUTTON)
-  await signButton.click()
-  console.log('🚀 ~ goJD ~ signButton:', '签到成功')
-  await browser.close()
-  process.exit(0)
+  try {
+    const signButton = await page.waitForSelector(JD_SIGN_BUTTON, {timeout: 1000})
+    await signButton.click()
+    await browser.close()
+    process.exit(0)
+  } catch (err) {
+    process.exit(1)
+  }
 }
